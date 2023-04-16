89095ac4254d0e5e648665d7b7b6122663afa3ec
Denis <dienis.labutin@mail.ru>
2023-04-16 19:29:14 +0300

    Adaptive layout

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,16 @@ const question_items = document.querySelectorAll('.question__item');
 question_items.forEach((item) => item.addEventListener('click', questionClick));
 
 function questionClick(e) {
-  //   const opened_items = document.querySelectorAll('.question__opened');
-  //   if (opened_items.length > 0) {
-  //     opened_items.forEach((item) => item.classList.remove('question__opened'));
-  //   }
-
-  e.currentTarget.classList.toggle('question__opened');
-  let current_icon = e.currentTarget.querySelector('.question__icon').innerText;
-  if (current_icon === '+') {
-    e.currentTarget.querySelector('.question__icon').innerText = '-';
-  } else {
+  const opened_item = document.querySelector('.question__opened');
+  if (opened_item == e.currentTarget) {
+    e.currentTarget.classList.remove('question__opened');
     e.currentTarget.querySelector('.question__icon').innerText = '+';
+  } else {
+    if (opened_item) {
+      opened_item.classList.remove('question__opened');
+      opened_item.querySelector('.question__icon').innerText = '+';
+    }
+    e.currentTarget.classList.add('question__opened');
+    e.currentTarget.querySelector('.question__icon').innerText = '-';
   }
-
-  //   setTimeout((e) => answerShow(e), 2000);
 }
-
-// function answerShow(e) {
-//   e.currentTarget.classList.toggle('question__show');
-// }
